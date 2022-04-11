999f57a65d271eed36863edd508414c962b32961
Aboneh Manyazewal <aboneh@gmail.com>
2022-04-11 17:45:36 -0500

    update on resolvers

---
+++ frontend/src/app/resolvers/cart-resolver.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Cart } from '../models/cart';
+import { AuthService } from '../services/auth/auth.service';
+import { CartService } from '../services/cart/cart.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartResolverService implements Resolve<Cart>{
+
+  constructor(private cartService: CartService, private authService: AuthService) { }
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Cart>{
+    if(this.authService.profile){
+      return this.cartService.getCart(this.authService.profile.cartId)
+    }
+  }
+}

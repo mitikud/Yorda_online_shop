f9c7a58bc77cc41267086cdacfe6e8484ff997bc
Aboneh Manyazewal <aboneh@gmail.com>
2022-04-13 23:45:59 -0500

    update commit

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from './models/category';
 import { AuthService } from './services/auth/auth.service';
+import { CategoryService } from './services/category/category.service';
 
 @Component({
   selector: 'app-root',
@@ -8,11 +10,23 @@ import { AuthService } from './services/auth/auth.service';
 })
 export class AppComponent implements OnInit{
   title = 'e-commerce';
-  constructor(public authService: AuthService){
+  categories!: Category[]; // will be sent to child component
+  inputCategories!: Category[];
+  constructor(public authService: AuthService, private categoryService: CategoryService){
     authService.prepareUserData();
+    this.prepareCategories();
+  }
+
+  prepareCategories(){
+    this.categoryService.getCategories()
+    .subscribe(resData => {
+      this.categories = resData;
+    })
   }
 
   ngOnInit(): void {
     this.authService.prepareUserData();
+    this.prepareCategories();
+
   }
 }
--- frontend/src/app/components/category-list/category-list.component.ts
+++ frontend/src/app/components/category-list/category-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {CategoryService} from "../../services/category/category.service";
+import {Category} from "../../models/category";
 
 @Component({
   selector: 'app-category-list',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category-list.component.css']
 })
 export class CategoryListComponent implements OnInit {
-
-  constructor() { }
+  categories!: Category[];
+  @Input()
+  inputCategories!: Category[]; // input variable
+  constructor(private route: ActivatedRoute, private router: Router,
+              private categoryService: CategoryService) {
+    if (this.route.snapshot.data['categories']) {
+      this.categories = this.route.snapshot.data['categories'];
+    }
+  }
 
   ngOnInit(): void {
   }
 
+  viewCategoryDetails(category: Category) {
+    this.router.navigate(['/categories', category.id], {
+      queryParams: {
+        Name: category.name
+      }
+    })
+  }
 }

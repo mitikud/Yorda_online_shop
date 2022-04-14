a1a693402e8a05c9f1bf27374262802c629e3f94
amigoscode <amigoscode.gmail>
2022-04-13 22:16:24 -0400

    remove un used things

--- frontend/src/app/models/cart-item.ts
+++
@@ -1,9 +0,0 @@
-import {Product} from "./product";
-import {Cart} from "./cart";
-
-export class CartItem {
-  id: number | undefined;
-  total_products: number | undefined;
-  products: Product[] | undefined;
-  cart: Cart | undefined;
-}
--- frontend/src/app/models/cart.ts
+++
@@ -1,7 +0,0 @@
-import {Profile} from "./profile";
-
-export class Cart {
-  id: number | undefined;
-  profile: Profile | undefined;
-  cartItemId: number | undefined;
-}
